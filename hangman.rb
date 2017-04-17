7500d3ea54988d269fdb7c1a64d40f5d24eb411f
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-04-17 19:08:37 -0400

    Create method to select random word from txt file

--- hangman.rb
+++ hangman.rb
@@ -1,1 +1,9 @@
-dictionary = File.readlines '5desk.txt'
+def select_random_word
+  word = File.readlines("c://sites/hangman/5desk.txt").sample.to_s
+if word.length == 4 || word.length <= 12
+  puts word
+else select_random_word
+end
+end
+
+puts select_random_word
