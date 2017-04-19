905b34fd5ed78d205d1b261f796872ec4f449864
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-04-18 21:00:26 -0400

    Create startGame method

--- hangman.rb
+++ hangman.rb
@@ -1,16 +1,32 @@
 class Hangman
 
 def select_random_word
-  word = File.readlines("c://sites/hangman/5desk.txt").sample.to_s
+  @word = File.readlines("c://sites/hangman/5desk.txt").sample.to_s
 
-if word.length == 4 || word.length <= 12
-  puts word
-else select_random_word
+  if @word.length == 4 || @word.length <= 12
+    @word
+  else select_random_word
+  end
+
 end
+
+def startGame
+  select_random_word
+
+  @length = @word.length
+  @hint = Array.new(@length){"_"}
+
+  print "Let's play Hangman!\nYou have 6 chances to guess my word.\nHere is your hint: #{@hint}\nGive me a letter!"
+  letter = gets.chomp
+  @word.each_char { |chr| if letter == chr
+    print "yes"
+  end  }
+
+
 
 end
 
 end
 
 a = Hangman.new
-puts a.select_random_word
+puts a.startGame
